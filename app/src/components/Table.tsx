f2c79754a1fcbeb759a0a40018dddc4569ff31aa
Russal Arya <russalarya@gmail.com>
2023-12-13 19:57:30 +1100

    Basic table

--- app/src/components/Table.tsx
+++ app/src/components/Table.tsx
@@ -1,6 +1,30 @@
 function Table() {
+    const cols = [
+        "Address",
+        "Weekly Rent",
+        "Public Transport",
+        "Cycling",
+        "Walk",
+        "Groceries (walk times)",
+        "Food",
+        "Size",
+        "Convenience",
+        "Ensuite?",
+        "Furnished?",
+        "Sharehouse?",
+        "Bills",
+        "Rented?"
+    ]
     return (
-        <></>
+        <>
+            <table className="table-auto rounded">
+                <tr>
+                    {cols.map(col => {
+                        return <th className="bg-indigo-500 px-10 py-2.5 text-white border-solid border-1 border-white">{col}</th>
+                    })}
+                </tr>
+            </table>
+        </>
     )
 }
 
