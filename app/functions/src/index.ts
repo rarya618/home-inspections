e3a4af2a5e39bf5a7425b63ecc6ca9acb20eaf43
Russal Arya <russalarya@gmail.com>
2026-05-28 06:59:46 +1000

    Updated region to us-central1

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,7 +1,7 @@
 import { onRequest } from "firebase-functions/v2/https";
 import { setGlobalOptions } from "firebase-functions/v2";
 
-setGlobalOptions({ region: "australia-southeast1" });
+setGlobalOptions({ region: "us-central1" });
 
 interface ListingData {
   address?: string;
