5a955fde3a8fb8557ac1370c0c3859f4fc070adc
Russal Arya <russalarya@gmail.com>
2026-06-09 15:50:27 +0530

    Add destinations file

---
+++ app/src/config/destinations.ts
@@ -0,0 +1,5 @@
+export const DESTINATIONS = {
+  uni:  'Peter Nicol Russell Building, 2 Butlin Ave, Darlington NSW 2008',
+  uts:  'Building 2, University of Technology Sydney, 61 Broadway, Ultimo NSW 2007',
+  work: 'The Star, 20-80 Pyrmont St, Pyrmont NSW 2009',
+}
